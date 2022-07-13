e0acea7ff253e4afe5576523754569449b392e8c
syobonoon <syobonoon@gmail.com>
2022-07-14 04:13:36 +0900

    update magic class

--- src/main/java/net/syobonoon/plugin/miclosmagic/magic/MagicDefinition.java
+++ src/main/java/net/syobonoon/plugin/miclosmagic/magic/MagicDefinition.java
@@ -1,4 +1,38 @@
 package net.syobonoon.plugin.miclosmagic.magic;
 
 public class MagicDefinition {
+    private String magicName;
+    private MagicAttribute magicAttribute;
+    private MagicType magicType; //攻撃、回復、防御など
+    private double magicMana;
+    private double magicAtk;
+
+    public MagicDefinition(String magicName, MagicAttribute magicAttribute, MagicType magicType, double magicMana, double magicAtk){
+        this.magicName = magicName;
+        this.magicAttribute = magicAttribute;
+        this.magicType = magicType;
+        this.magicMana = magicMana;
+        this.magicAtk = magicAtk;
+    }
+
+    public String getMagicName() {
+        return this.magicName;
+    }
+
+    public MagicAttribute getMagicAttribute() {
+        return this.magicAttribute;
+    }
+
+    public MagicType getMagicType() {
+        return this.magicType;
+    }
+
+    public double getMagicMana() {
+        return this.magicMana;
+    }
+
+    public double getMagicAtk() {
+        return this.magicAtk;
+    }
+
 }
